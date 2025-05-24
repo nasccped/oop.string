6dec66fbbddb1e1e0134219ef9d76d806e65d4c1
NASCC <pedro.main.tec@gmail.com>
2025-05-24 10:59:42 -0300

    chore: move main to a new naming schema

---
+++ src/00-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../core/banner.h"
+
+char *SECTION_SUBTITLE = "Main";
+
+int main(int argc, char *argv[]) {
+  print_banner(SECTION_SUBTITLE);
+}
