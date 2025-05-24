b55c695f25e9a12c2183eaadb9996c781defe1b3
NASCC <pedro.main.tec@gmail.com>
2025-05-24 11:28:00 -0300

    chore: add description printing

--- src/00-main.c
+++ src/00-main.c
@@ -2,10 +2,19 @@
 #include <stdlib.h>
 
 #include "../core/banner.h"
+#include "../core/colors.h"
 
 char *SECTION_SUBTITLE = "Main";
 
 int main(int argc, char *argv[]) {
   print_banner(SECTION_SUBTITLE);
+  printf(
+    "Welcome to the %sOOP%s.%sString%s, a repository to share most of my\n" // row 1
+    "%sString%s related knowledge in a low level way (%sC lang%s)!\n",      // row 2
+    // row 1
+    GREEN_NONE, WHITE_NONE, YELLOW_NONE, RESET,
+    // row 2
+    YELLOW_NONE, RESET, BLUE_NONE, RESET
+  );
   return 0;
 }
