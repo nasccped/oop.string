30babe84a31e0f427f98f0880b297545582eb6fd
NASCC <pedro.main.tec@gmail.com>
2025-05-24 10:57:09 -0300

    chore: add visual cores (coloring + banner printing)

---
+++ core/colors.c
@@ -0,0 +1,7 @@
+#include "../core/colors.h"
+
+const char *RESET       = "\x1b[0m"    ;
+const char *GREEN_NONE  = "\x1b[92m"   ;
+const char *YELLOW_NONE = "\x1b[93m"   ;
+const char *WHITE_NONE  = "\x1b[97m"   ;
+const char *WHITE_BLUE  = "\x1b[97;44m";
---
+++ core/colors.h
@@ -0,0 +1,13 @@
+#ifndef _CORE_COLORS
+#define _CORE_COLORS
+
+// avoid double including
+#pragma once
+
+extern const char *RESET;
+extern const char *GREEN_NONE;
+extern const char *YELLOW_NONE;
+extern const char *WHITE_NONE;
+extern const char *WHITE_BLUE;
+
+#endif // !_CORE_COLORS
