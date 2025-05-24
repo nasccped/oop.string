3a4807a83d6d520c5983c46127a78fae944ec837
NASCC <pedro.main.tec@gmail.com>
2025-05-23 21:40:00 -0300

    chore: add little script to dev test

--- src/main.c
+++ src/main.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 
+const char *BLUE  = "\x1b[37;44m";
+const char *RESET = "\x1b[0m"    ;
+
+// test new functions
+int add_ints(int a, int b);
+
 int main(int argc, char *argv[]) {
-  if (argc > 1) {
-    char *arg = argv[1];
-    printf("The first arg is: %s\n", arg);
-  } else
-    printf("No arg was given!\n");
-  return 0;
+  int a, b;
+  printf("Say an integer: ");
+  scanf("%d", &a);
+  printf("Say another integer: ");
+  scanf("%d", &b);
+
+  printf("The sum of them is %s %d %s\n",
+         BLUE, add_ints(a, b), RESET);
+}
+
+int add_ints(int a, int b) {
+  return a + b;
 }
