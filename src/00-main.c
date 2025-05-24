e41b712476678c34cb495cf6f5f4176ced987848
NASCC <pedro.main.tec@gmail.com>
2025-05-24 12:15:02 -0300

    chore: move cur message to default message func

--- src/00-main.c
+++ src/00-main.c
@@ -6,26 +6,32 @@
 
 #include "../headers/00-main.h"
 
-char *SECTION_SUBTITLE = "00-Main";
-
 int main(int argc, char *argv[]) {
   // if no args
   if (argc < 2) {
     default_message();
     return 0;
   }
+  printf(
+    "\nDefinitely not a default message %s:^D%s\n" // row 1
+    "Arg handling %sshould be implemented%s!\n\n", // row 2
+    // row 1
+    BLUE_NONE, RESET,
+    // row 2
+    GREEN_NONE, RESET);
+  return 0;
+}
+
+char *SECTION_SUBTITLE = "00-Main";
+
+void default_message() {
   print_banner(SECTION_SUBTITLE);
   printf(
     "Welcome to the %sOOP%s.%sString%s, a repository to share most of my\n" // row 1
-    "%sString%s related knowledge in a low level way (%sC lang%s)!\n",      // row 2
+    "%sString%s related knowledge in a low level way (%sC lang%s)!\n\n",    // row 2
     // row 1
     GREEN_NONE, WHITE_NONE, YELLOW_NONE, RESET,
     // row 2
     YELLOW_NONE, RESET, BLUE_NONE, RESET
   );
-  return 0;
-}
-
-void default_message() {
-  printf("This is the default message!\n");
 }
