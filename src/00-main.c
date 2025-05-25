b653a8e73b3b599a6c28367b0cd00ca252fe8ba5
NASCC <pedro.main.tec@gmail.com>
2025-05-25 10:19:32 -0300

    chore: implement arg handler initial feats + messages

--- src/00-main.c
+++ src/00-main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "../core/arg_handler.h"
 #include "../core/banner.h"
 #include "../core/colors.h"
 
 #include "../headers/00-main.h"
 
 int main(int argc, char *argv[]) {
+  ARG_HANDLER.set_fields(&ARG_HANDLER, argc, argv);
   // if no args
-  if (argc < 2) {
+  if (!ARG_HANDLER.have_args(&ARG_HANDLER)) {
     default_message();
     return 0;
   }
@@ -33,5 +35,17 @@ void default_message() {
     GREEN_NONE, WHITE_NONE, YELLOW_NONE, RESET,
     // row 2
     YELLOW_NONE, RESET, BLUE_NONE, RESET
+  );
+  printf(
+    "Here's the init point (%sas you can see%s in title `%s%s%s`).\n"           // row 1
+    "Also, there's an %sArgument Handler%s. This program work as an %sCLI%s.\n" // row 2
+    "Try it by passing `%shelp%s` after the binary\n"                           // row 3
+    "path! (e.g., path/to/bin help)\n\n",
+    // row 1
+    RED_NONE, RESET, YELLOW_NONE, SECTION_SUBTITLE, RESET,
+    // row 2
+    BLUE_NONE, RESET, BLUE_NONE, RESET,
+    // row 3
+    YELLOW_NONE, RESET
   );
 }
